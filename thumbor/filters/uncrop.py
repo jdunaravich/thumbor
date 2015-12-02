cd141953460d2c2e8b6b2ead0e2f57ea70c46fbb
Joseph Dunaravich <joseph.dunaravich@intelligent.com>
2015-12-01 17:26:20 -0800

    Include uncrop filter

---
+++ thumbor/filters/uncrop.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# thumbor imaging service
+# https://github.com/thumbor/thumbor/wiki
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 globo.com timehome@corp.globo.com
+
+from thumbor.filters import BaseFilter, filter_method
+
+
+class Filter(BaseFilter):
+
+    @filter_method(BaseFilter.Number, BaseFilter.Number,
+                   BaseFilter.Number, BaseFilter.Number)
+    def uncrop(self, top=0, right=0, bottom=0, left=0):
+        if not any([padding for padding in (top, right, bottom, left)]):
+            return
+        upper_left_pixel = self.engine.get_upper_left_pixel()
+        self.engine.uncrop(upper_left_pixel=upper_left_pixel, top=top,
+                           right=right, bottom=bottom, left=left)
